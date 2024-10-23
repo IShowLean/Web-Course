4780cdbe4fd59149a5aff181a97bce35c3a31725
IShowLean <dobrikov.egor@mail.ru>
2024-10-23 19:12:51 +0300

    JS functionality added

---
+++ scripts/main.js
@@ -0,0 +1,38 @@
+const menuButton = document.getElementById("menuButton");
+const navMenu = document.getElementById("navMenu");
+
+menuButton.addEventListener("click", function () {
+    navMenu.classList.toggle("open");
+});
+
+(function () {
+    window.addEventListener('load', function () {
+        const [navigationEntry] = performance.getEntriesByType("navigation");
+
+        const loadTime = navigationEntry.domContentLoadedEventEnd - navigationEntry.startTime;
+
+        const footer = document?.querySelector('footer');
+        const copyright = footer?.querySelector('p');
+
+        if (copyright) {
+            copyright.innerHTML += `<br>Время загрузки страницы: ${loadTime.toFixed(2)} мс`;
+            copyright.style.textAlign = 'center';
+            copyright.style.fontSize = '14px'
+            copyright.style.color = 'white'
+            copyright.style.fontFamily = "Montserrat Alternates";
+        }
+        console.log(`Время загрузки страницы: ${loadTime.toFixed(2)} мс`)
+    });
+})();
+
+(function () {
+    const currentPath = window.location.pathname;
+    const menuItems = document.querySelectorAll('.nav-menu a.nav');
+
+    menuItems.forEach(function (menuItem) {
+        const href = menuItem.getAttribute('href');
+        if (currentPath.includes(href)) {
+            menuItem.classList.add('active');
+        }
+    });
+})();
